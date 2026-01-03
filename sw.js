96c7ae6e0416d6cbd85aec4f4c366df319c89c5a
correo5v51-bit <correo5v51@gmail.com>
2026-01-02 23:47:32 -0600

    Update service worker to use new cache and assets

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const CACHE_NAME = 'v_pro_v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
+  'https://raw.githubusercontent.com/correo5v51-bit/vacaciones-/main/10861844.png'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('v1').then(cache => cache.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
+
